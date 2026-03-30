a8483be466b9bd42f66a70264e6ff02fb749c53c
hyeonmin <haeron1102@gmail.com>
2026-03-30 23:36:36 +0900

    확장 프로그램 수정본 2차

--- crawling.js
+++ crawling.js
@@ -1,13 +1,14 @@
-const cheerio = require('cheerio');
-
 async function getHtml() {
-    const original_html = await fetch("https://klms.kaist.ac.kr/");
-    const modified_html = await original_html.text();
+    // const original_html = await fetch("https://klms.kaist.ac.kr/");
+    // const modified_html = await original_html.text();
 
-    console.log(modified_html);
+    // console.log(modified_html);
 
-    const $ = cheerio.load(modified_html);
-    console.log($(".main-course-list.student").text());    
+    const urls = document.querySelectorAll('.fullname')
+    
+    for(let i = 0; i < urls.length; i++) {
+        console.log(urls[i].href)
+    }
 }
 
 getHtml();
--- popup.js
+++ popup.js
@@ -1,5 +1,16 @@
-document.getElementById("crawl").addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id, { action: "crawl" });
-    });
+async function getHtml() {
+    // const original_html = await fetch("https://klms.kaist.ac.kr/");
+    // const modified_html = await original_html.text();
+
+    // console.log(modified_html);
+
+    const urls = document.querySelectorAll('.fullname')
+    
+    for(let i = 0; i < urls.length; i++) {
+        console.log(urls[i].href)
+    }
+}
+    
+document.querySelector('#crawlBtn').addEventListener("click", () => {
+    getHtml();
 });
