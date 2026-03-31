c0ea3dd7091f00a5cc9e1e5c1494d3d18144170a
hyeonmin <haeron1102@gmail.com>
2026-03-31 13:07:28 +0900

    return 공지사항 주소

--- popup.js
+++ popup.js
@@ -1,5 +1,7 @@
 document.querySelector('#crawlBtn').addEventListener("click", async () => {
-    const [tab] = await chrome.tabs.query( {active : true, currentWindow : true });
+        const [tab] = await chrome.tabs.query( {active : true, currentWindow : true });
 
     chrome.tabs.sendMessage(tab.id, { action : "crawl"} );
+
+    document.querySelector('#result').innerText = "잠시만 기다려주세요..";
 });
