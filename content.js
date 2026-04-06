c7c93fade47be6ea93bf605a6bb6ee5b768e65da
hyeonmin <haeron1102@gmail.com>
2026-04-06 14:27:14 +0900

    동기화

--- content.js
+++ content.js
@@ -1,9 +1,25 @@
- chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+async function getHtml(url) {
+    const class_html = await fetch(url);
+    const text_html = await class_html.text();
+
+    const parser = new DOMParser();
+    const dom_html = parser.parseFromString(text_html, "text/html");
+
+    const notice_url = dom_html.querySelector('.activity.courseboard .aalink');
+    
+    if (!notice_url) {
+        return null
+    } else {
+        return notice_url.href         
+    }
+}
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "crawl") {
-        const urls = document.querySelectorAll('.fullname');
+        const class_urls = document.querySelectorAll('.fullname');
         
-        for (let i = 0; i < urls.length; i++) {
-            console.log(urls[i].href);
+        for(let i = 0; i < class_urls.length; i++) {
+            getHtml(class_urls[i].href);
         }
     }
 });
